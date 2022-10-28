12819bdce72865bc044c512181ba1e4db798f5a4
risalstr <risa.lestari2002@gmail.com>
2022-10-28 22:59:30 +0700

    first commit

--- src/components/elements/Post.tsx
+++ src/components/elements/Post.tsx
@@ -1,124 +1,186 @@
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
-import { Modal, ModalOverlay, ModalContent, ModalHeader, 
-  ModalFooter, ModalBody, ModalCloseButton, Button, Input, Select, useToast, useDisclosure, Textarea, } from '@chakra-ui/react'
-import { useState, } from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Input,
+  useDisclosure,
+  Textarea,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import React from 'react'
 
-const Post = (props:any) => {
-    let hour, day
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [name, setname] = useState(props.name)
-    const [image, setImage] = useState("")
-    const [imagePreview, setImagePreview] = useState(props.image)
-    const [description, setDescription] = useState(props.description)
-    const [date, setDate] = useState(props.expired_date)
+const Post = (props: any) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [name, setname] = useState(props.name)
+  const [image, setImage] = useState('')
+  const [imagePreview, setImagePreview] = useState(props.image)
+  const [description, setDescription] = useState(props.description)
+  const [date, setDate] = useState(props.expired_date)
 
-    var trimmedString = props.name.substr(0, 16)
-    var desc = props.description.slice(0, 20) + (props.description.length > 20 ? "..." : "");    
-    if(props.name.length > trimmedString.length){ /* Apabila jumlah huruf > 21 maka akan ditambahkan ... */
-        trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + " ..."
-    }else{ /* Apabila lebih kecil dari 21 maka name nya akan seperti itu*/
-        trimmedString = props.name
+  let trimmedString = props.name.substr(0, 16)
+  const desc =
+    props.description.slice(0, 20) +
+    (props.description.length > 20 ? '...' : '')
+  if (props.name.length > trimmedString.length) {
+    /* Apabila jumlah huruf > 21 maka akan ditambahkan ... */
+    trimmedString =
+      trimmedString.substr(
+        0,
+        Math.min(trimmedString.length, trimmedString.lastIndexOf(' '))
+      ) + ' ...'
+  } else {
+    /* Apabila lebih kecil dari 21 maka name nya akan seperti itu*/
+    trimmedString = props.name
+  }
+
+  const createdTimeStamp = props.expired_date.substring(0, 10) + ' WIB '
+
+  const handleImageUpload = (e: any) => {
+    try {
+      setImage(e.target.files[0])
+      setImagePreview(URL.createObjectURL(e.target.files[0]))
+    } catch (error) {
+      setImagePreview(
+        'https://icons.veryicon.com/png/o/education-technology/alibaba-cloud-iot-business-department/image-load-failed.png'
+      )
     }
-    
-    const createdTimeStamp = props.expired_date.substring(0, 10) + " WIB "
+  }
 
-    
-    const handleImageUpload = (e: any) => {
-      try{
-        setImage(e.target.files[0]);
-        setImagePreview(URL.createObjectURL(e.target.files[0]))
-      } catch(error){
-        setImagePreview("https://icons.veryicon.com/png/o/education-technology/alibaba-cloud-iot-business-department/image-load-failed.png")
+  const handleUpdate = () => {
+    if (name === '' && description === '' && date === '') {
+      props.showToast(
+        'Invalid Input',
+        'Ada input yang belum terisi',
+        'error',
+        'top-center'
+      )
+    } else {
+      if (image === '') {
+        props.updateToAPI(name, props.id, null, description, date)
+      } else {
+        props.updateToAPI(name, props.id, image, description, date)
+        setImage('')
       }
     }
+  }
 
-    const handleUpdate = () => {
-      if(name === "" && description === "" && date === ""){
-        props.showToast("Invalid Input", "Ada input yang belum terisi", "error", "top-center")
-      }else{  
-        if(image === ""){
-          props.updateToAPI(name, props.id, null, description, date)
-        }else{
-          props.updateToAPI(name, props.id, image, description, date)
-          setImage("")
-        }
-      }
-    }
+  return (
+    <>
+      <div className="mx-8 my-12 hover:scale-105 duration-100 flex">
+        <div className="flex bg-white border border-gray-300 rounded-xl md:flex-row flex-col overflow-hidden items-center justify-start">
+          <div className="relative w-64 h-64 md:w-32 md:h-32 flex-shrink-0">
+            <div className="absolute left-0 top-0 w-full h-full block">
+              <img
+                className="w-full block max-w-full h-full object-cover"
+                alt="gambar"
+                src={props.image}
+              />
+            </div>
+          </div>
 
-    return (
-      <>
-        <div className="mx-8 my-12 hover:scale-105 duration-100 flex">
-            <div className="flex bg-white border border-gray-300 rounded-xl md:flex-row flex-col overflow-hidden items-center justify-start">                     
-                <div className="relative w-64 h-64 md:w-32 md:h-32 flex-shrink-0">                      
-                    <div className="absolute left-0 top-0 w-full h-full block">                                    
-                        <img className = "w-full block max-w-full h-full object-cover" alt = "gambar" src = {props.image}/>
-                    </div>
-                </div>
-                                        
-              <div className="px-4 flex flex-col md:mt-0 mt-2 md:text-left text-center">                         
-                  <p className="text-xl capitalize text-ellipsis"> {trimmedString} </p>        
-                  <p className="text-xl text-gray-300 text-[10px]"> {desc} </p>                                            
-                  <p className="text-xs text-green-500"> {createdTimeStamp} </p>
-                  <div className = "flex mt-4 justify-evenly">
-                    <div className = "flex flex-col items-center">
-                      <EditIcon _hover={{ color: "#48BB78"}} color= "green" className = "cursor-pointer mb-1 duration-700" onClick={onOpen}/>
-                      <p className = "text-xs"> Edit </p> 
-                    </div>
-                    <div className = "flex flex-col items-center justify-center md:mb-0 mb-4">
-                      <DeleteIcon _hover={{ color: "#F56565"}} onClick = {props.handleDeletePost} color= "red" className = "cursor-pointer mb-1 duration-700"/>
-                      <p className = "text-xs"> Delete </p> 
-                    </div>
-                  </div>             
+          <div className="px-4 flex flex-col md:mt-0 mt-2 md:text-left text-center">
+            <p className="text-xl capitalize text-ellipsis">
+              {' '}
+              {trimmedString}{' '}
+            </p>
+            <p className="text-xl text-gray-300 text-[10px]"> {desc} </p>
+            <p className="text-xs text-green-500"> {createdTimeStamp} </p>
+            <div className="flex mt-4 justify-evenly">
+              <div className="flex flex-col items-center">
+                <EditIcon
+                  _hover={{ color: '#48BB78' }}
+                  color="green"
+                  className="cursor-pointer mb-1 duration-700"
+                  onClick={onOpen}
+                />
+                <p className="text-xs"> Edit </p>
+              </div>
+              <div className="flex flex-col items-center justify-center md:mb-0 mb-4">
+                <DeleteIcon
+                  _hover={{ color: '#F56565' }}
+                  onClick={props.handleDeletePost}
+                  color="red"
+                  className="cursor-pointer mb-1 duration-700"
+                />
+                <p className="text-xs"> Delete </p>
               </div>
             </div>
+          </div>
         </div>
+      </div>
 
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader> Update Roti </ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-                <div className = "flex flex-col">
-                  <label className = "font-bold"> Bread name </label>
-                  <Input value = {name} className = "my-4" placeholder = "Masukkan nama roti" onChange={(e) => setname(e.target.value)}/>
-                </div>
-                <div className = "flex flex-col">
-                  <label className = "font-bold"> Bread Description </label>
-                  <Textarea value = {description} className = "my-4" placeholder = "Masukkan deskripsi roti" onChange={(e) => setDescription(e.target.value)}/>
-                </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader> Update Roti </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="flex flex-col">
+              <label className="font-bold"> Bread name </label>
+              <Input
+                value={name}
+                className="my-4"
+                placeholder="Masukkan nama roti"
+                onChange={e => setname(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-bold"> Bread Description </label>
+              <Textarea
+                value={description}
+                className="my-4"
+                placeholder="Masukkan deskripsi roti"
+                onChange={e => setDescription(e.target.value)}
+              />
+            </div>
 
-                <div className = "flex flex-col">
-                  <label className = "font-bold"> Bread's Expiring Date  </label>
-                  <input  type="date" value = {date} className = "my-4" placeholder = "Masukkan nama roti" onChange={(e) => setDate((e.target.value))}/>
-                </div>
+            <div className="flex flex-col">
+              <label className="font-bold"> Bread&apos;s Expiring Date </label>
+              <input
+                type="date"
+                value={date}
+                className="my-4"
+                placeholder="Masukkan nama roti"
+                onChange={e => setDate(e.target.value)}
+              />
+            </div>
 
-                <div className = "flex flex-col">
-                  <label className = "font-bold"> Bread Image </label>
-                  <img alt = "gambar" src = {imagePreview} />
-                  <input className = "my-4" type = "file" accept="image/jpeg, image/png" onChange = {handleImageUpload}/>
-                </div>
-            </ModalBody>
+            <div className="flex flex-col">
+              <label className="font-bold"> Bread Image </label>
+              <img alt="gambar" src={imagePreview} />
+              <input
+                className="my-4"
+                type="file"
+                accept="image/jpeg, image/png"
+                onChange={handleImageUpload}
+              />
+            </div>
+          </ModalBody>
 
-            <ModalFooter>
-              <Button
-                color = 'white' 
-                variant='solid' 
-                borderRadius='6px' 
-                borderColor='#22c55e' 
-                bg = "#22c55e"
-                mr={3} 
-                onClick={handleUpdate}
-                _hover={{ boxShadow: '0 0 1px 2px rgba(88, 144, 255, .75)'}} 
-              >
-                Update
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
+          <ModalFooter>
+            <Button
+              color="white"
+              variant="solid"
+              borderRadius="6px"
+              borderColor="#22c55e"
+              bg="#22c55e"
+              mr={3}
+              onClick={handleUpdate}
+              _hover={{ boxShadow: '0 0 1px 2px rgba(88, 144, 255, .75)' }}
+            >
+              Update
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
 }
 
 export default Post
- 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,189 +1,264 @@
 import type { NextPage } from 'next'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { Button, useToast, useDisclosure, } from '@chakra-ui/react'
+import { Button, useToast, useDisclosure } from '@chakra-ui/react'
 import Modals from '../components/elements/Modals'
 import Post from '../components/elements/Post'
+import React from 'react'
 
 const Home: NextPage = () => {
-    const [posts, setPosts] = useState<any[]>([])
-    const [status, setStatus] = useState(false)
-    const tokenStr = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY5NDgyMTQxLCJpYXQiOjE2NjY4OTAxNDEsImp0aSI6ImJhNmE2Y2NiYTk1MDQxOTI4OTQzZTllZjQ3NzVmZjNlIiwidXNlcl9pZCI6Mn0.mAX6l50k5v1eu-k9uO__HQ02kdfqZtC9y6MWBimZF4g"
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const toast = useToast()
+  const [posts, setPosts] = useState<any[]>([])
+  const [status, setStatus] = useState(false)
+  const tokenStr =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY5NDgyMTQxLCJpYXQiOjE2NjY4OTAxNDEsImp0aSI6ImJhNmE2Y2NiYTk1MDQxOTI4OTQzZTllZjQ3NzVmZjNlIiwidXNlcl9pZCI6Mn0.mAX6l50k5v1eu-k9uO__HQ02kdfqZtC9y6MWBimZF4g'
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
 
-    const showToast = (name: any, error: any, status: any, position: any) => {
-      toast({
-        title: name,
-        description: error,
-        status: status,
-        duration: 5000,
-        isClosable: true,
-        position: position,
+  const showToast = (name: any, error: any, status: any, position: any) => {
+    toast({
+      title: name,
+      description: error,
+      status: status,
+      duration: 5000,
+      isClosable: true,
+      position: position,
+    })
+  }
+
+  const getFromAPI = () => {
+    axios
+      .get(`https://betis23-oprec.herokuapp.com/roti/`, {
+        headers: {
+          Authorization: `Bearer ${tokenStr}`,
+        },
       })
-    }
 
-    const getFromAPI = () => {
-        axios.get(`https://betis23-oprec.herokuapp.com/roti/`, {
-            headers: {
-                Authorization: `Bearer ${tokenStr}`,
-            }
-        })
+      .then(result => {
+        const data = result.data.data
+        console.log(data)
+        setPosts(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
-        .then((result) => {
-            var data = result.data.data
-            console.log(data)
-            setPosts(data)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  function postToAPI(name: any, description: any, image: any, date: any) {
+    const data = new FormData()
 
-    }
+    data.append('name', name)
+    data.append('description', description)
+    data.append('image', image)
+    data.append('expired_date', date)
 
-    function postToAPI(name: any, description: any, image: any, date: any){
-        const data = new FormData()
-  
-        data.append("name", name)
-        data.append("description", description)
-        data.append("image", image)
-        data.append("expired_date", date)
+    axios
+      .post('https://betis23-oprec.herokuapp.com/roti/', data, {
+        headers: {
+          Authorization: `Bearer ${tokenStr}`,
+        },
+      })
 
-        axios.post("https://betis23-oprec.herokuapp.com/roti/", data, {
-          headers:{
-            Authorization: `Bearer ${tokenStr}`
-          }
-        })
-  
-        .then((result) => {
-          console.log(result)
+      .then(result => {
+        console.log(result)
+        setStatus(!status)
+        showToast(
+          'Post Uploaded',
+          'Post berhasil di upload!',
+          'success',
+          'top-center'
+        )
+      })
+
+      .catch(error => {
+        showToast(
+          'Input Error / Server Error',
+          'Input salah atau server tidak tersambung',
+          'error',
+          'top-center'
+        )
+      })
+  }
+
+  const updateToAPI = (
+    name: string,
+    id: number,
+    image: any,
+    description: any,
+    date: any
+  ) => {
+    if (image === null) {
+      const payload = {
+        name: name,
+        description: description,
+        expired_date: date,
+      }
+
+      const config = {
+        method: 'patch',
+        url: `https://betis23-oprec.herokuapp.com/roti/${id}`,
+        headers: {
+          Authorization: `Bearer ${tokenStr}`,
+        },
+        data: payload,
+      }
+
+      axios(config)
+        .then(result => {
           setStatus(!status)
-          showToast("Post Uploaded", "Post berhasil di upload!", "success", "top-center")
+          showToast(
+            'Post Updated',
+            'Post berhasil di update!',
+            'success',
+            'top-center'
+          )
         })
-  
-        .catch((error) =>{
-            showToast("Input Error / Server Error", "Input salah atau server tidak tersambung", "error", "top-center")
+
+        .catch(error => {
+          showToast(
+            'Input Error / Server Error',
+            'Input salah atau server tidak tersambung',
+            'error',
+            'top-center'
+          )
+        })
+    } else {
+      const data = new FormData()
+
+      data.append('name', name)
+      data.append('description', description)
+      data.append('image', image)
+      data.append('expired_date', date)
+
+      const config = {
+        method: 'patch',
+        url: `https://betis23-oprec.herokuapp.com/roti/${id}`,
+        headers: {
+          Authorization: `Bearer ${tokenStr}`,
+        },
+        data: data,
+      }
+
+      axios(config)
+        .then(result => {
+          setStatus(!status)
+          showToast(
+            'Post Updated',
+            'Post berhasil di update!',
+            'success',
+            'top-center'
+          )
+        })
+
+        .catch(error => {
+          console.log(error)
+          showToast(
+            'Input Error / Server Error',
+            'Input salah atau server tidak tersambung',
+            'error',
+            'top-center'
+          )
         })
     }
+  }
+  const handleDeletePost = (id: Number) => {
+    axios
+      .delete(`https://betis23-oprec.herokuapp.com/roti/${id}`, {
+        headers: {
+          Authorization: `Bearer ${tokenStr}`,
+        },
+      })
+      .then(result => {
+        setStatus(!status)
+        showToast(
+          'Post deleted',
+          'Post sudah berhasil dihilangkan!',
+          'success',
+          'top-center'
+        )
+      })
+      .catch(error => {
+        showToast(
+          'Failed to delete',
+          'Post tidak berhasil untuk dihilangkan!',
+          'error',
+          'top-center'
+        )
+      })
+  }
 
-    const updateToAPI = (name: string, id: number, image: any, description: any, date: any) => {
-        if(image === null){
+  useEffect(() => {
+    getFromAPI()
+  }, [status])
 
-            let payload = {
-                name: name,
-                description: description,
-                expired_date: date,
-            }
+  return (
+    <div className="font-bold">
+      <div className="mx-6 my-4 flex flex-col items-center">
+        <h1 className="text-3xl text-center text-[#fdfffe]">
+          {' '}
+          Open Recruitement BETIS 2023{' '}
+        </h1>
 
-            const config = {
-                method: "patch",
-                url: `https://betis23-oprec.herokuapp.com/roti/${id}`,
-                headers:{
-                    Authorization: `Bearer ${tokenStr}`
-                },
-                data: payload,
-            }
-            
-            axios(config)
+        <Button
+          className="my-4"
+          onClick={onOpen}
+          color="white"
+          borderColor="#22c55e"
+          bg="#22c55e"
+          variant="solid"
+          borderRadius="6px"
+          _hover={{ boxShadow: '0 0 1px 2px rgba(88, 144, 255, .75)' }}
+          _active={{
+            bg: '#22c55e',
+            transform: 'scale(1.03)',
+            borderColor: '#bec3c9',
+          }}
+        >
+          Upload Your Bread Here!
+        </Button>
 
-            .then((result) => {
-                setStatus(!status)
-                showToast("Post Updated", "Post berhasil di update!", "success", "top-center")
-            })
+        <Modals
+          isOpen={isOpen}
+          onClose={onClose}
+          tokenStr={tokenStr}
+          postToAPI={(name: any, description: any, image: any, date: any) =>
+            postToAPI(name, description, image, date)
+          }
+        />
 
-            .catch((error) => {
-                showToast("Input Error / Server Error", "Input salah atau server tidak tersambung", "error", "top-center")
-            })
-
-        }else{  
-            const data = new FormData()
-
-            data.append("name", name)
-            data.append("description", description)
-            data.append("image", image)
-            data.append("expired_date", date)
-
-            const config = {
-                method: "patch",
-                url: `https://betis23-oprec.herokuapp.com/roti/${id}`,
-                headers:{
-                    Authorization: `Bearer ${tokenStr}`
-                },
-                data: data,
-            }
-            
-            axios(config)
-
-            .then((result) => {
-                setStatus(!status)
-                showToast("Post Updated", "Post berhasil di update!", "success", "top-center")
-            })
-
-            .catch((error) => {
-                console.log(error)
-                showToast("Input Error / Server Error", "Input salah atau server tidak tersambung", "error", "top-center")
-            })
-        }
-    }
-    const handleDeletePost = (id: Number) => {
-        axios.delete(`https://betis23-oprec.herokuapp.com/roti/${id}`, {
-            headers: {
-                Authorization: `Bearer ${tokenStr}`
-            }
-        })
-        .then((result) => {
-            setStatus(!status)
-            showToast("Post deleted", "Post sudah berhasil dihilangkan!", "success", "top-center")
-        })
-        .catch((error) => {
-            showToast("Failed to delete", "Post tidak berhasil untuk dihilangkan!", "error", "top-center")
-        })
-
-    }
-
-    useEffect(() => {
-        getFromAPI()
-    }, [status])
-
-    return (
-        <div className = "font-bold">
-            <div className = "mx-6 my-4 flex flex-col items-center">
-                <h1 className = "text-3xl text-center text-[#fdfffe]"> Open Recruitement BETIS 2023 </h1> 
-
-                <Button 
-                    className = "my-4"
-                    onClick={onOpen}
-                    color = 'white' 
-                    borderColor='#22c55e' 
-                    bg = "#22c55e" 
-                    variant='solid' 
-                    borderRadius='6px' 
-                    _hover={{ boxShadow: '0 0 1px 2px rgba(88, 144, 255, .75)' }} 
-                    _active={{bg: '#22c55e', transform: 'scale(1.03)', borderColor: '#bec3c9',}}
-                > 
-                    Upload Your Bread Here! 
-                </Button>
-
-                <Modals isOpen = {isOpen} onClose = {onClose} tokenStr = {tokenStr} postToAPI = {(name:any, description:any, image:any, date:any) => postToAPI(name, description, image, date)}/>
-
-                {posts.length >= 1 ?
-                    <div className = "max-w-screen-xl w-full flex md:flex-row flex-col flex-wrap justify-center items-center">
-                        {posts.map((post) => {
-                            return <Post key = {post.id} name = {post.name} description = {post.description}
-                            image = {post.image} expired_date = {post.expired_date} handleDeletePost = {() => handleDeletePost(post.id)} showToast = {(name: any, error: any, status:any, position: any) => showToast(name, error, status, position)} updateToAPI = {updateToAPI} id = {post.id}/>
-                        })}
-                    </div>
-                 : 
-                    (<div> 
-                        <h1 className = "text-3xl my-16 font-bold text-white"> Nothing to show Here </h1> 
-                    </div>)
-                }
-            </div>
-
-        </div>
-    )
-
+        {posts.length >= 1 ? (
+          <div className="max-w-screen-xl w-full flex md:flex-row flex-col flex-wrap justify-center items-center">
+            {posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  name={post.name}
+                  description={post.description}
+                  image={post.image}
+                  expired_date={post.expired_date}
+                  handleDeletePost={() => handleDeletePost(post.id)}
+                  showToast={(
+                    name: any,
+                    error: any,
+                    status: any,
+                    position: any
+                  ) => showToast(name, error, status, position)}
+                  updateToAPI={updateToAPI}
+                  id={post.id}
+                />
+              )
+            })}
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-3xl my-16 font-bold text-white">
+              {' '}
+              Nothing to show Here{' '}
+            </h1>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
- 
